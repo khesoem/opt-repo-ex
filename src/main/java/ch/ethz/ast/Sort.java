17bab9e7f16f8212eebea573675d70c6741ca3f3
khaes-kth <khaes@kth.se>
2025-10-02 18:45:52 +0200

    first commit

---
+++ src/main/java/ch/ethz/ast/Sort.java
@@ -0,0 +1,36 @@
+package ch.ethz.ast;
+
+import java.util.Arrays;
+
+public class Sort 
+{
+
+    public void bubbleSort(int[] arr) {
+        if (arr == null || arr.length <= 1) {
+            return;
+        }
+        int n = arr.length;
+        for (int i = 0; i < n - 1; i++) {
+            boolean swapped = false;
+            for (int j = 0; j < n - i - 1; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    // Swap arr[j] and arr[j + 1]
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                    swapped = true;
+                }
+            }
+            // If no two elements were swapped by inner loop, then break
+            if (!swapped) {
+                break;
+            }
+        }
+    }
+
+    public void sort(int[] arr) {
+        bubbleSort(arr);
+    }
+    public static void main( String[] args ) {
+    }
+}
---
+++ src/test/java/ch/ethz/ast/SortTest.java
@@ -0,0 +1,31 @@
+package ch.ethz.ast;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SortTest {
+
+    @Test
+    @DisplayName("Should sort array in ascending order")
+    void testSort() {
+        // Arrange
+        Sort sort = new Sort();
+        int[] arr = new int[30000];
+        java.util.Random rand = new java.util.Random();
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = rand.nextInt();
+        }
+        
+        // Act
+        sort.sort(arr);
+        
+        // Assert - check that array is sorted in ascending order
+        for (int i = 0; i < arr.length - 1; i++) {
+            assertTrue(arr[i] <= arr[i + 1], 
+                "Array is not sorted: arr[" + i + "] = " + arr[i] + " > arr[" + (i + 1) + "] = " + arr[i + 1]);
+        }
+    }
+
+}
